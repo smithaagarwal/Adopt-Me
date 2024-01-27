9a3967a78f63437e83c28d12605c2d1aefa9a874
smithaagarwal <smitha.kotian@gmail.com>
2024-01-27 22:29:43 +0000

    first commit

---
+++ adopt-me/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Adopt Me</h1>
+    </>
+  )
+}
+
+export default App
